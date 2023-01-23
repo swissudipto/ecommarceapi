2dfbe6fa00841c99eab1f12e6f3bd101df6e41af
Sudipto Bose <44774203+swissudipto@users.noreply.github.com>
2023-01-23 15:33:09 +0530

    Signup configured

--- BlackBox/Sellerlogic.cs
+++ BlackBox/Sellerlogic.cs
@@ -11,19 +11,26 @@ public class sellerlogic : ISellerlogic
     }
     public string signup(signupmodel signupobj)
     {
-      var Userobj=context.UserCreds.Where(x=> x.Username==signupobj.Username);
-      if(Userobj!=null)
-      {
-        /*if(Userobj.Password==signupobj.password)
-        {
-        return "Success";
-        }
-        else
-        {
-          return "Wrong password";
-        }*/
-      }
+      //var Userobj=context.UserCreds.Where(x=> x.Username==signupobj.Username);
+      try{
+      var maxuserid=context.Usercreds.DefaultIfEmpty().Max(r => r == null ? 0 : r.Userid);
 
-        return "User Name is Incorrect";
+      Usercred newuser=new Usercred(){
+          Userid=maxuserid+1,
+          Username=signupobj.Username,
+          Email=signupobj.email,
+          Password=signupobj.password
+      };
+
+      context.Usercreds.Add(newuser);
+      context.SaveChanges();
+      }
+      catch(Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+        return "Something went Wrong"+ex.Message;
+      }
+          
+        return "User is created";
     }
 }
